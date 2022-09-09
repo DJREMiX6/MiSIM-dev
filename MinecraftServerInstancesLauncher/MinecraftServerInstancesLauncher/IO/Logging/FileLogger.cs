3746257d73262c84b1eb31e1224e33838e22867f
FG <francescogirellilime@gmail.com>
2022-09-10 00:12:49 +0200

    Finished FileLogger (Tests needed)

--- MinecraftServerInstancesLauncher/MinecraftServerInstancesLauncher/IO/Logging/FileLogger.cs
+++ MinecraftServerInstancesLauncher/MinecraftServerInstancesLauncher/IO/Logging/FileLogger.cs
@@ -1,6 +1,8 @@
-﻿using MinecraftServerInstancesLauncher.MinecraftServerInstanceManagement.Interpretation;
+﻿using MinecraftServerInstancesLauncher.IO.Logging.LogFileManagement;
+using MinecraftServerInstancesLauncher.MinecraftServerInstanceManagement.Interpretation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -10,40 +12,86 @@ namespace MinecraftServerInstancesLauncher.IO.Logging
 {
     public class FileLogger : ILogger
     {
-        //TODO
+
+        LogFileGate logFileGate = LogFileGate.Instance;
+
+        #region ILogger IMPLEMENTATION
+
         public void Log(string message)
         {
-            throw new NotImplementedException();
+            WriteToLogFileGate(LogFileFormatter.FormatDefaultLog(message));
         }
 
         public void LogError(string message)
         {
-            throw new NotImplementedException();
+            WriteToLogFileGate(LogFileFormatter.FormatErrorLog(message));
         }
 
         public void LogInfo(string message)
         {
-            throw new NotImplementedException();
+            WriteToLogFileGate(LogFileFormatter.FormatInfoLog(message));
         }
 
         public void LogWaring(string message)
         {
-            throw new NotImplementedException();
+            WriteToLogFileGate(LogFileFormatter.FormatWarningLog(message));
         }
+
+        #endregion ILogger IMPLEMENTATION
+
+        #region IMinecraftServerInterpretedDataReceiver IMPLEMENTATION
 
         public void MinecraftServerOutputInterpretedDataReceived(object sender, MinecraftServerInterpretedOutputData data)
         {
-            throw new NotImplementedException();
+            switch (data.Type)
+            {
+                case MinecraftServerOutputType.INFO:
+                    LogInfo(data.Data);
+                    break;
+                case MinecraftServerOutputType.WARNING:
+                    LogWaring(data.Data);
+                    break;
+                case MinecraftServerOutputType.ERROR:
+                    LogError(data.Data);
+                    break;
+                case MinecraftServerOutputType.DEFAULT:
+                    Log(data.Data);
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException($"{data.Type} is not a valid {nameof(MinecraftServerOutputType)}.");
+            }
         }
+
+        #endregion IMinecraftServerInterpretedDataReceiver IMPLEMENTATION
+
+        #region IProcessDataReceiver IMPLEMENTATION
 
         public void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (e.Data is not null)
+            {
+                LogError(e.Data);
+            }
         }
 
         public void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (e.Data is not null)
+            {
+                Log(e.Data);
+            }
         }
+
+        #endregion IProcessDataReceiver IMPLEMENTATION
+
+        #region PRIVATE METHODS
+
+        private void WriteToLogFileGate(string log)
+        {
+            logFileGate.Write(log);
+        }
+
+        #endregion PRIVATE METHODS
+
     }
 }
--- MinecraftServerInstancesLauncher/MinecraftServerInstancesLauncher/IO/Logging/LogFileManagement/LogFileGate.cs
+++ MinecraftServerInstancesLauncher/MinecraftServerInstancesLauncher/IO/Logging/LogFileManagement/LogFileGate.cs
@@ -1,4 +1,6 @@
-﻿namespace MinecraftServerInstancesLauncher.IO.Logging.LogFileManagement
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace MinecraftServerInstancesLauncher.IO.Logging.LogFileManagement
 {
     public class LogFileGate
     {
@@ -13,36 +15,83 @@
         #region PRIVATE FIELDS
 
         private FileInfo _logFileInfo;
-        private List<LogFileGateActionWrapper> _logFileGateActions;
+        private Queue<string> _logQueue;
+        private bool _isWriting = false;
 
         #endregion PRIVATE FIELDS
+
+        #region PRIVATE PROPS
+
+        private FileInfo logFileInfo => _logFileInfo ??= new FileInfo(ConstantsAbstraction.Instance.LOG_FILE_FULL_PATH);
+        private Queue<string> logQueue => _logQueue ??= new Queue<string>();
+
+        #endregion PRIVATE PROPS
 
         #region CTORS
 
         private LogFileGate()
-        {
-            _logFileInfo = new(ConstantsImplementation.Instance.LOG_FILE_FULL_PATH);
-            _logFileGateActions = new();
-            //TODO EVENTS SUBSCRIPTION
-        }
+        { }
 
         #endregion CTORS
 
-        public async void Write(string text)
+        #region PUBLIC METHODS
+
+        public async void Write(string log)
         {
-            using (var writer = new StreamWriter(_logFileInfo.FullName, true))
+            lock (logQueue)
             {
-                await writer.WriteAsync(text);
+                logQueue.Enqueue(log);
+                OnEnqueued();
             }
         }
 
-        public async void WriteLine(string text)
+        public async void WriteLine(string log)
+        {
+            Write(log + Environment.NewLine);
+        }
+
+        #endregion PUBLIC METHODS
+
+        #region PRIVATE METHODS
+
+        private void OnEnqueued()
+        {
+            if(!_isWriting)
+            {
+                WriteLogIntoLogFile();
+            }
+        }
+
+        private bool IsLogQueueEmpty()
+        {
+            return logQueue.Count == 0;
+        }
+
+        private async void WriteLogIntoLogFile()
         {
             using (var writer = new StreamWriter(_logFileInfo.FullName, true))
             {
-                await writer.WriteLineAsync(text);
+                while (!IsLogQueueEmpty())
+                {
+                    StartedWriting();
+                    string log = logQueue.Dequeue();
+                    await writer.WriteAsync(log);
+                }
             }
+            StoppedWriting();
         }
+
+        private void StartedWriting()
+        {
+            _isWriting = true;
+        }
+
+        private void StoppedWriting()
+        {
+            _isWriting = false;
+        }
+
+        #endregion PRIVATE METHODS
 
     }
 }
